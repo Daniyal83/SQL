43098e67d1fd0d9ea1bb84c71f1176dd41131dea
Valeriy <voronglass84@gmail.com>
2017-11-21 22:06:38 +0300

    Task 2 Create a query to return a list ordered by order id descending:
    | Order Id | Order Total Price | Total Order Discount, % |

--- tasks.sql
+++ tasks.sql
@@ -4,3 +4,15 @@
 -- [Employee Full Name] - First Name and Last Name.
 SELECT E.EmployeeID, E.FirstName, E.LastName, E.Title, E.City FROM Employees E ORDER BY E.City
 SELECT E.EmployeeID, E.FirstName, E.LastName, E.Title, E.City FROM Employees E ORDER BY E.FirstName
+
+--Second task
+--Create a query to return a list ordered by order id descending
+--| Order Id | Order Total Price | Total Order Discount, % |
+SELECT OD.OrderID, (OD.UnitPrice * OD.Quantity) AS OrderTotalPrice, OD.Discount AS [TotalOrderDiscount, %]
+FROM [Order Details] OD
+ORDER BY OD.OrderID DESC
+-- alternative
+SELECT OD.OrderID, SUM(OD.UnitPrice) AS TotalCount, SUM(OD.Discount)*100 AS TotalDiscount 
+FROM [Order Details] OD
+GROUP BY OD.OrderId 
+ORDER BY OD.OrderID DESC
